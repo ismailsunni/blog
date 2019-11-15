925132f28ef23c4d6def0447796d0ecf799e5a94
Ismail Sunni <imajimatika@gmail.com>
2019-11-15 17:55:02 +0100

    First commit.

---
+++ config.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: 'Blog by Ismail Sunni',
+  subtitle: 'Lorem Ipsum dolot',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'U',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/'
+    },
+    {
+      label: 'About me',
+      path: '/pages/about'
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts'
+    }
+  ],
+  author: {
+    name: 'Ismail Sunni',
+    photo: '/photo.jpg',
+    bio: 'A [geospatial] software engineer',
+    contacts: {
+      email: 'imajimatika@gmail.com',
+      facebook: '#',
+      telegram: 'ismailsunni',
+      twitter: 'ismailsunni',
+      github: 'ismailsunni',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: 'ismail.sunni',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: ''
+    }
+  }
+};
